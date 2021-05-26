c73311ea00de989d5a9c018309ecceed371ad3c9
Engin Toprak <engin.toprak1@ogr.sakarya.edu.tr>
2021-05-26 23:57:28 +0300

    updated

--- HRMS/src/main/java/kodlamaio/HRMS/api/controllers/UsersController.java
+++ HRMS/src/main/java/kodlamaio/HRMS/api/controllers/UsersController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +32,7 @@ public class UsersController {
 	}
 	
 	@PostMapping("/verify")
-	public Result verify(String email, String verifyCode) {
+	public Result verify(@RequestBody String email, String verifyCode) {
 		return userService.verifyUser(email, verifyCode);
 	}
 	
